f5600cefcb1f288d48e491c23d6978c53989eced
Siva M <siva.m@ramco.com>
2020-05-25 20:40:42 +0530

    added static file serving for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./db").connectDB;
+const path = require("path");
 
 const PORT = process.env.port || 6000;
 
@@ -10,16 +11,21 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-	res.send("App is running......");
-});
-
 // Define Routes
 
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log("Server started at port ", PORT);
